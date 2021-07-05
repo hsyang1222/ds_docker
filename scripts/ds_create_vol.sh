2d9184ed160634be2d041db9fadfcd8ec13ba595
hsyang1222 <hsyang1222@snu.ac.kr>
2021-07-05 08:23:31 -0400

    create vol

--- scripts/ds_create_vol.sh
+++ scripts/ds_create_vol.sh
@@ -1,11 +1,12 @@
 # Team mapping
 HOSTNAME=`hostname`
 case $HOSTNAME in
-    "ds1") TEAMS=(1 2 3) ;;
-    "ds2") TEAMS=(4 5 6) ;;
-    "ds3") TEAMS=(7 8 9) ;;
-    "ds4") TEAMS=(10 11 12) ;;
-    "ds5") TEAMS=(3 6 9 12) ;;
+    "ds1") TEAMS=(1 2) ;;
+    "ds2") TEAMS=(3 4) ;;
+    "ds3") TEAMS=(5 6) ;;
+    "ds4") TEAMS=(7 8) ;;
+    "ds5") TEAMS=(9 10) ;;
+    "ds6") TEMAS=(11 12) ;;
 esac
 
 for (( i=0; i<${#TEAMS[@]}; i++ ));
